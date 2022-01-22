8790947aff541df116c50357e68b4cdc548846d8
KalpeshDongarkar <kalpeshdongarkar67@gmail.com>
2022-01-22 21:34:27 +0530

    Updated file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserdataComponent } from './form/userdata/userdata.component';
+import { HomeComponent } from './main/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'form', component: UserdataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserdataComponent } from './form/userdata/userdata.component';
+import { HomeComponent } from './main/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserdataComponent
+    UserdataComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
