61acaa9f0eea2861822f5b49831f1d6c51e117ae
KalpeshDongarkar <kalpeshdongarkar67@gmail.com>
2022-01-22 20:14:52 +0530

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserdataComponent } from './form/userdata/userdata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserdataComponent
   ],
   imports: [
     BrowserModule,
